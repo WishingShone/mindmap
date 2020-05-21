1445cb4e4ca40c9c2ef0a05fe7975cf2bf3d2af4
迷hotel <syx19921022@163.com>
2020-05-21 22:15:00 +0800

    主分支

--- src/views/MindNode.vue
+++ src/views/MindNode.vue
@@ -17,6 +17,7 @@
 
 <script>
 // 移除键盘监听，通过鼠标操作思维导图
+import { uuid } from 'vue-uuid'
 import JSONData from '../JSONData'
 import * as d3 from 'd3'
 // import outline from '../components/Outline'
@@ -53,7 +54,7 @@ export default {
       if (!sele.nodes()[0]) {
         return;
       }
-      const newJSON = { name: '新建节点', children: [] };
+      const newJSON = { name: '新建节点', uuid:uuid.v1() ,children: [] };
       const keyName = event.key;
       if (keyName === 'Tab') { // 添加子节点
         event.preventDefault();
